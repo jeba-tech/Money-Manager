9887b2457a8cb2666431ca1582bb2d8a2f9d8b32
JEBA <fawjiajeba@gmail.com>
2022-02-17 21:14:47 +0600

    balance added

--- js/calculation.js
+++ js/calculation.js
@@ -12,14 +12,29 @@ function updateTotalExpense(total, amount) {
       const expensesText = expenses.innerText;
       const convertExpensesText = parseFloat(expensesText);
       expenses.innerText = convertExpensesText + amount;
+
+
+
 }
 
 
+function updateBalance(amount) {
+      const balance = document.getElementById('balance');
+      const balanceText = balance.innerText;
+      const convertBalanceText = parseFloat(balanceText);
+      balance.innerText = convertBalanceText - amount;
+}
 
 document.getElementById('calc-button').addEventListener('click', function () {
       // handle input event  
+      const income = getInputValue('income-input');
       const amount = getInputValue('food-input') + getInputValue('rent-input') + getInputValue('clothes-input');
 
       //handle total expense
       updateTotalExpense('total-expenses', amount);
+
+      updateTotalExpense('balance', income);
+      updateBalance(amount);
+
+
 })
