f683714a6c1e8154f51858ac339384562438fec3
JEBA <fawjiajeba@gmail.com>
2022-02-18 02:08:22 +0600

    calculation done

--- js/calculation.js
+++ js/calculation.js
@@ -17,24 +17,68 @@ function updateTotalExpense(total, amount) {
 
 }
 
-
-function updateBalance(amount) {
+function updateNewBalance() {
       const balance = document.getElementById('balance');
       const balanceText = balance.innerText;
       const convertBalanceText = parseFloat(balanceText);
-      balance.innerText = convertBalanceText - amount;
+      return convertBalanceText;
 }
 
+function updateBalance(amount) {
+      const balance = document.getElementById('balance');
+
+      const convertBalanceText = updateNewBalance();
+      balance.innerText = convertBalanceText - amount;
+
+}
+
+
+
+function getSave(total, amount) {
+      const save = document.getElementById(total);
+      const saveText = save.innerText;
+      const convertSaveText = parseFloat(saveText);
+      save.innerText = convertSaveText + (amount * 100);
+
+}
+
+function remainingBalance(input) {
+      const savee = document.getElementById('remaining-amount');
+      const saveText = savee.innerText;
+      const convertSaveText = parseFloat(saveText);
+      const updatebalance = updateNewBalance();
+      savee.innerText = convertSaveText + updatebalance - (input * 100);
+}
 document.getElementById('calc-button').addEventListener('click', function () {
       // handle input event  
       const income = getInputValue('income-input');
+      if (income > 0) {
+            //handle balance event
+
+            updateTotalExpense('balance', income);
+
+      }
       const amount = getInputValue('food-input') + getInputValue('rent-input') + getInputValue('clothes-input');
+      if (amount > 0) {
+            //handle total expense event
+            updateTotalExpense('total-expenses', amount);
+            updateBalance(amount);
+      }
 
-      //handle total expense
-      updateTotalExpense('total-expenses', amount);
 
-      updateTotalExpense('balance', income);
-      updateBalance(amount);
+});
+
+
+document.getElementById('save-button').addEventListener('click', function () {
+      const save = getInputValue('save-input');
+      if (save > 0) {
+            getSave('saving', save);
+            debugger;
+
+            remainingBalance(save);
+
+
+      }
 
 
 })
